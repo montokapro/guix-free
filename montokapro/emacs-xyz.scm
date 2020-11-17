5979432a9887fcf4dedf7397a066d2e1aa026d3a
Stephen Webber <montokapro@gmail.com>
2020-11-16 22:10:28 -0600

    Upgrade helm-emacs-projectile

--- montokapro/emacs-xyz.scm
+++ montokapro/emacs-xyz.scm
@@ -13,6 +13,31 @@
 ;;; Emacs hacking.
 ;;;
 
+(define-public emacs-helm-projectile
+  (let ((commit "2f3a2a03d6cb9419c25b432637aa11c8d2f9f3b7")
+        (revision "1"))
+    (package
+      (name "emacs-helm-projectile")
+      (version (git-version "1.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/bbatsov/helm-projectile")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0h6r8v2lj6abjz73iv8568ijs7l37j76nf58h4p9r9ldpdigihzz"))))
+      (build-system emacs-build-system)
+      (propagated-inputs
+       `(("emacs-helm" ,emacs-helm)
+         ("emacs-projectile" ,emacs-projectile)))
+      (home-page "https://github.com/bbatsov/helm-projectile")
+      (synopsis "Helm integration for Projectile")
+      (description
+       "This Emacs library provides a Helm interface for Projectile.")
+      (license license:gpl3+))))
+
 (define-public emacs-sbt-mode
   (package
     (name "emacs-sbt-mode")
